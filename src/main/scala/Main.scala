04baa60d9341277b8f229975bdc84e95ef978af3
LemonHX <runzhe2001@hotmail.com>
2018-10-07 19:17:13 +0200

    Removed commet of which priv edition has left

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -13,57 +13,6 @@ import shader.Shader
 
 import scala.util.Random
 object Main extends App {
-  /*val vec1 = Vec(1,3)
-  val vec2 = new Vec(0,1,-2)
-  val vec3 = new Vec(2,3,4)
-  println((vec1*vec2).angle(vec3))
-  println("now try to make z with x and y to verify the cross")
-  val x = new Vec(1,0,0)
-  val y = new Vec(0,1,0)
-  val z = x.cross(y)
-  println(s" ${z.toString} and the angle between is ${x.angle(y)}")*/
-
-  //  def hit(center:Vec,radius:Float,r: Ray):Float = {
-  //    val oc = r.origin - center
-  //    val a:Float = r.direction().dot(r.direction())
-  //    val b:Float = oc.dot(r.direction())*2.0f
-  //    val c:Float = oc.dot(oc) - radius*radius
-  //    val d:Float = b*b - 4*a*c
-  //    if(d<0){
-  //      return -1.0f
-  //    }else{
-  //      return (-b - Math.sqrt(d).toFloat)/(2.0f*a)
-  //    }
-  //  }
-  //  def color(r: Ray):Vec = {
-  //    val t = hit(Vec(0,0,-1),0.5f,r)
-  //    if(t>0){
-  //      val n:Vec = (r.pointAtScale(t)-Vec(0,0,-1f)).unitVec()
-  //      return Vec(n.x+1f,n.y+1f,n.z+1f)*0.5f
-  //    }else{
-  //     val ud:Vec = r.direction().unitVec()
-  //      val t = (ud.y+1f) * 0.5f
-  //      return Vec(1f)*(1f-t) + Vec(0.5f,0.7f,1.0f) * t.toFloat
-  //    }
-  //  }
-
-  //  def hit(center:Vec,radius:Float,r:Ray): Boolean ={
-  //    val oc = r.origin - center
-  //        val a:Float = r.direction().dot(r.direction())
-  //        val b:Float = (oc.dot(r.direction())*2.0).toFloat
-  //        val c:Float = oc.dot(oc) - radius*radius
-  //        val d:Float = b*b - 4*a*c
-  //        return d>0
-  //  }
-  //  def color(r: Ray):Vec = {
-  //      if(hit(Vec(0,0,-1),0.5f,r)){
-  //        return Vec(1,0,0)
-  //      }
-  //    val ud = r.direction().unitVec()
-  //    val t:Float = (ud.y+1)*0.5f
-  //    return (Vec(1)*(1.0f-t) + Vec(0.5f,0.7f,1.0f)*t)
-  //  }
-
   override def main(args: Array[String]): Unit = {
     val nx = 1000
     val ny = 500
